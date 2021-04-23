8e0d2d9c01e08fc06282b4affffea9abe5789f9c
wongtawee <wongtawiaut@gmail.com>
2021-04-23 20:30:51 +0700

    second commit

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+  app = express(),
+  passport = require("passport"),
+  port = process.env.PORT || 80,
+  cors = require("cors"),
+  cookie = require("cookie");
+
+const bcrypt = require("bcrypt");
+
+const db = require("./database.js");
+let users = db.users;
+
+require("./passport.js");
+
+const router = require("express").Router(),
+  jwt = require("jsonwebtoken");
